d70b834bf65874addae2a8188e52198e67289736
Hyphie <31455641+Hyphie24@users.noreply.github.com>
2024-09-01 16:13:01 -0400

    Update WarWithin.lua

--- DB/Data/WarWithin.lua
+++ DB/Data/WarWithin.lua
@@ -5,3 +5,95 @@ local data = {
         name = "War Within"
 }
 WQA.data[11] = data
+
+-- Achievements
+data.achievements = {
+	{
+        name = "Worm Theory",
+        id = 40869,
+        criteriaType = "QUESTS",
+        criteria = {
+            { 82324 },
+			{ 79959 },
+			{ 79958 }
+        }
+    },
+    {
+        name = "I Only Need One Trip",
+        id = 40623,
+        criteriaType = "QUEST_SINGLE",
+        criteria = {
+            { 82580 }
+        }
+    },
+	{
+        name = "For the Collective",
+        id = 40630,
+        criteriaType = "QUESTS",
+        criteria = {
+            { 82580 }
+        }
+    },
+	{
+        name = "Hanging Tight",
+        id = 40507,
+        criteriaType = "QUEST_SINGLE",
+        criteria = {
+            { 83101 }
+        }
+    },
+	{
+        name = "Panhandled",
+        id = 40731,
+        criteriaType = "QUESTS",
+        criteria = {
+            { 83101 }
+        }
+    },
+	{
+        name = "Children's Entertainer",
+        id = 40150,
+        criteriaType = "QUESTS",
+        criteria = {
+            { 82288 }			
+        }
+    },
+	{
+        name = "Children's Entertainer",
+        id = 40150,
+        criteriaType = "QUESTS",
+        criteria = {
+            { 82288 }			
+        }
+    },
+	{
+        name = "A Champion's Tour: The War Within",
+        id = 40088,
+        criteriaType = "QUESTS",
+        criteria = {
+            { 80395 },
+            { 13608 },
+			{ 80208 },
+			{ 80323 },
+			{ 80412 },
+			{ 81622 },
+            { 80409 },
+            { 80457 }
+        }
+    },
+    {
+        name = "Battle on Khaz Algar",
+        id = 40153,
+        criteriaType = "QUESTS",
+        criteria = {
+            82293,
+            82292,
+            82300,
+            82294,
+            82298,
+            82291,
+            82295,
+            82297
+        }
+    },   
+}
